445036ced64dfbfa0a7841a33a891eacf962e4fe
polyovik <taisiia.boroda@gmail.com>
2023-01-15 13:48:17 +0200

    updated databases homework week1

--- databases/week1/homework.sql
+++ databases/week1/homework.sql
@@ -8,7 +8,7 @@ SELECT COUNT(id) FROM task WHERE due_date IS NULL;
 SELECT title FROM task LEFT JOIN status ON task.status_id = status.id WHERE status.name = 'Done';
 
 /* 4. Find all the tasks that are not marked as done */
-SELECT title FROM task LEFT JOIN status ON task.status_id = status.id WHERE status.name != 'Done';
+SELECT task.title FROM task LEFT JOIN status ON task.status_id = status.id WHERE status.name != 'Done';
 
 /* 5. Get all the tasks, sorted with the most recently created first */
 SELECT * FROM task ORDER BY created DESC;
@@ -23,7 +23,7 @@ SELECT * FROM task WHERE title LIKE '%database%' OR description  LIKE '%database
 SELECT task.title, status.name FROM task LEFT JOIN status ON task.status_id = status.id; 
 
 /* 9. Get the name of each status, along with a count of how many tasks have that status */
-SELECT COUNT(task.id), status.name FROM task LEFT JOIN status ON task.status_id = status.id GROUP BY task.status_id;
+SELECT COUNT(task.id), status.name FROM task LEFT JOIN status ON task.status_id = status.id GROUP BY status.name;
 
 /* 10. Get the names of all statuses, sorted by the status with most tasks first */
 SELECT COUNT(task.status_id), status.name FROM task LEFT JOIN status ON task.status_id = status.id GROUP BY task.status_id ORDER BY task.status_id DESC;
